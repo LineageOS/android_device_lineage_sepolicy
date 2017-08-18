0927139965011b16f89b593fcc0b08d87b2947f3
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from Lineage
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/qcom
