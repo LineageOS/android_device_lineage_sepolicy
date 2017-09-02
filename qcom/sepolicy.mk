19c0f93e8fa9fab8be68ec82e650f9e7dba0820d
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-02 10:05:15 +0800

    sepolicy: Add QCOM sepolicy from CAF
    * Tag: LA.UM.6.4.r1-03200-8x98.0.
    
    * Includes changes from https://github.com/LineageOS/android_device_qcom_sepolicy cm-14.1
    Commits:
    
    eba6ea57b78fda25dd76017c7720f7c425b46263 - Correct executable bit for source files
    Author: chirayudesai
    
    Change-Id: I71b3bb9c0e6a8d733e13cfccb49e33409b211724
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -4,4 +4,18 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom
+    device/lineage/sepolicy/qcom \
+    device/lineage/sepolicy/qcom/common \
+    device/lineage/sepolicy/qcom/ssg \
+    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+BOARD_SEPOLICY_DIRS += \
+    device/qcom/lineage/sepolicy/qcom/test
+endif
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
+    device/lineage/sepolicy/qcom/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+    device/lineage/sepolicy/qcom/private
