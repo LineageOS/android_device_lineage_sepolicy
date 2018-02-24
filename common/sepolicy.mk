670d7c01b0f3c36a39ea3aa5282996fb761f1cb4
Paul Keith <javelinanddart@gmail.com>
2018-02-24 21:39:42 +0100

    common: Add sf_lcd_density_prop type and labelled props
    * Fixes sepolicy for recovery for devices that don't
      include device/qcom/sepolicy (like Nexus/Pixel)
    
    Change-Id: Iae1e15b524514ab30634b5e896603c43616faebc

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,4 +10,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/lineage/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/lineage/sepolicy/common/vendor \
+    device/lineage/sepolicy/common/common
