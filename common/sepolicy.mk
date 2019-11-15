ccc533f6ab0747535ac6fdc958b6dffcf91b0ef0
theimpulson <aayushgupta219@gmail.com>
2019-11-15 03:52:14 +0000

    sepolicy: Label and address Trust's system variant denials
    Change-Id: I33e3320da346e83b3c74e5631c20f556bd202c2b

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -17,7 +17,8 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic
+    device/lineage/sepolicy/common/dynamic \
+    device/lineage/sepolicy/common/system
 else
 BOARD_SEPOLICY_DIRS += \
     device/lineage/sepolicy/common/dynamic \
