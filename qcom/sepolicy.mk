7936c51be96d5b4c953cd2ce92463c729eaacce8
LuK1337 <priv.luk@gmail.com>
2020-09-19 09:51:38 +0800

    qcom: Align types with QCOM upstream using BOARD_SEPOLICY_M4DEFS
    Change-Id: I3bca9b0455d9f391ead84ba9ef2a1765afd939dd

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -26,3 +26,15 @@ ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,
 BOARD_SEPOLICY_DIRS += \
     device/lineage/sepolicy/qcom/legacy-vendor
 endif
+
+ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+BOARD_SEPOLICY_M4DEFS += \
+    qdisplay_service=vendor_qdisplay_service \
+    sysfs_graphics=vendor_sysfs_graphics \
+    hal_keymaster_qti_exec=vendor_hal_keymaster_qti_exec \
+    persist_block_device=vendor_persist_block_device \
+    display_vendor_data_file=vendor_display_vendor_data_file \
+    hal_perf_default=vendor_hal_perf_default \
+    sysfs_battery_supply=vendor_sysfs_battery_supply \
+    sysfs_usb_supply=vendor_sysfs_usb_supply
+endif
