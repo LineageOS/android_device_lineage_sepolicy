256fac8426a403a5beedd3839a9c2ee41cb59e34
Aaron Kling <webgeek1234@gmail.com>
2021-08-10 11:11:16 -0500

    atv: Allow setupwraith configs to be set by vendor
    Change-Id: I0e2831019d5bbb9d432bbad48cd91c98f3c3cb6f
    (cherry picked from commit 72049da6bc559ad5e57a2824b975fba5102258a0)

---
+++ atv/sepolicy.mk
@@ -0,0 +1,9 @@
+#
+# This policy configuration will be used by atv products that
+# inherit from Lineage
+#
+
+ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+BOARD_SEPOLICY_DIRS += \
+    device/lineage/sepolicy/atv/vendor
+endif
