0a82b049f089dfdff4ab0dd7f11bcafb4453bc9a
Michael Bestas <mkbestas@gmail.com>
2023-05-11 19:23:36 +0300

    qcom: Restrict access to /sys/devices/soc0/serial_number
    Change-Id: I6254ef6e160ff0d3c3ce2e51f20f557e75826dff

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -36,5 +36,6 @@ BOARD_SEPOLICY_M4DEFS += \
     qdisplay_service=vendor_qdisplay_service \
     sysfs_battery_supply=vendor_sysfs_battery_supply \
     sysfs_graphics=vendor_sysfs_graphics \
+    sysfs_socinfo_sensitive=vendor_sysfs_soc_sensitive \
     sysfs_usb_supply=vendor_sysfs_usb_supply
 endif
