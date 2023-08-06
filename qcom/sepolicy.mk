c2b77e67cebeaacd5b4e995f7e459a68ac1a33a0
Edwin Moquete <edwinmmoquete@gmail.com>
2023-08-06 03:18:59 +0000

    qcom: Remove sdm845 from the m4defs exclusion
    Guard m4defs with TARGET_USES_NONLEGACY_SEPOLICY instead,
    which will be set on device/qcom/sepolicy_vndr
    
    Change-Id: I1037fb2ed6c1f54f1bb480d23891476c7358b796

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -22,7 +22,8 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/qcom/vendor
 endif
 
-ifeq (,$(filter msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter msm8937 msm8953 msm8996 msm8998 sdm660 sdm710, $(TARGET_BOARD_PLATFORM)))
+ifneq ($(TARGET_USES_NONLEGACY_SEPOLICY), true)
 BOARD_SEPOLICY_M4DEFS += \
     display_vendor_data_file=vendor_display_vendor_data_file \
     hal_gnss_qti=vendor_hal_gnss_qti \
@@ -38,6 +39,7 @@ BOARD_SEPOLICY_M4DEFS += \
     sysfs_scsi_host=vendor_sysfs_scsi_host \
     sysfs_socinfo_sensitive=vendor_sysfs_soc_sensitive \
     sysfs_usb_supply=vendor_sysfs_usb_supply
+endif
 else
 BOARD_SEPOLICY_M4DEFS += \
     location_domain=location
