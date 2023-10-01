2413d8c9100a6fc038ce9cbab038b2fb5d6e2ec8
me-cafebabe <me.cafebabe@gmail.com>
2023-10-01 19:22:51 +0800

    sepolicy: qcom: Allow hal_gnss_qti to read xtra-daemon control property
    Fixes the following SELinux denial:
    
    [   13.227324] type=1400 audit(1696156342.168:13): avc: denied { read } for comm="Loc_hal_worker" name="u:object_r:xtra_control_prop:s0" dev="tmpfs" ino=15652 scontext=u:r:hal_gnss_qti:s0 tcontext=u:object_r:xtra_control_prop:s0 tclass=file permissive=0
    
    Change-Id: Ieeaa03577825ab07c5bffc6ef88021833d7833ad

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -25,6 +25,7 @@ endif
 ifeq (,$(filter msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
 BOARD_SEPOLICY_M4DEFS += \
     display_vendor_data_file=vendor_display_vendor_data_file \
+    hal_gnss_qti=vendor_hal_gnss_qti \
     hal_keymaster_qti_exec=vendor_hal_keymaster_qti_exec \
     hal_perf_default=vendor_hal_perf_default \
     location_domain=vendor_location \
