c869b95aa5a9fdcaa72160c2bf5bdc76e187a5f1
Han Sol Jin <hansol@hansol.ca>
2024-05-17 21:05:28 -0700

    sepolicy: guard libperfmgr policies
    Allow it to be an option to use libperfmgr-related policies that are
    present in this repository.
    
    Change-Id: I8457b65a07cbdd969d92b3e2618d2ed10819eebd

--- libperfmgr/sepolicy.mk
+++ libperfmgr/sepolicy.mk
@@ -1,4 +1,7 @@
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
+ifeq ($(TARGET_USES_LINEAGE_POWERHAL), true)
+$(warning USING LINEAGE POWERHAL)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/libperfmgr/vendor
+endif
 endif
