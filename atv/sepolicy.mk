04c08c8df0e536b3bceff5b25a8894eab900a7ea
Nolen Johnson <johnsonnolen@gmail.com>
2026-02-18 19:41:21 -0500

    atv: Update from newest DroidLogic devkit
    * Move existing public/private sepolicy to system_ext, as
      that's where these apps live.
    * `BOARD_SEPOLICY_DIRS` -> `BOARD_VENDOR_SEPOLICY_DIRS`.
    
    Change-Id: If8877d899fdadb2dc516db86dfe1df95a3015ae5

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -4,9 +4,12 @@
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/atv/vendor
 endif
 
-PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/system_ext/private
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+   device/lineage/sepolicy/atv/system_ext/public
